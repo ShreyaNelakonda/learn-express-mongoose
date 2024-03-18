fc7d8c4dcedc5c33efcd74931a89cfc5de3ca483
Shreya-Nelakonda <nelakonda.s@northeastern.edu>
2024-03-18 14:23:29 -0400

    03142024

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,18 @@
 let BookInstance = require('../models/bookinstance');
 
+function getAllBooks(){
+  return ;
+}
+
 exports.show_all_books_status = function(res) {
-  return res.send([]);
+  //return res.send([]);
+  BookInstance.find({'status': {$eq: 'Available'}})
+      .populate('book')
+      .exec()
+      .then(books => {
+        res.send (books.map(function (bookinstance) {
+          return bookinstance.book.title + ": " + bookinstance.status
+        }))
+      })
+      .catch(err => res.send('Status not found'))
 }
